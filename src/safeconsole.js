37de9e6a9fb69cc38acc087f04840889789420bd
fengtong <fengtong@360.cn>
2018-09-25 17:50:25 +0800

    :bug: always use global console

--- src/safeconsole.js
+++ src/safeconsole.js
@@ -1,13 +1,18 @@
-exports.hasConsole = function() {
-  if (global.console) {
-    return true
+exports.getConsole = function() {
+  if (typeof global !== 'undefined') {
+    return global.console
   }
-  return false
+}
+
+var globalConsole = exports.globalConsole = exports.getConsole()
+
+exports.hasConsole = function() {
+  return !!exports.globalConsole
 }
 
 exports.console = function(level, arr) {
   // support ie8+
   // http://stackoverflow.com/questions/5538972/console-log-apply-not-working-in-ie9
-  var apply = Function.prototype.apply || console[level].apply // wechat has no Function.prototype.apply
-  apply.call(console[level], console, arr)
+  var apply = Function.prototype.apply || globalConsole[level].apply // wechat has no Function.prototype.apply
+  apply.call(globalConsole[level], globalConsole, arr)
 }
